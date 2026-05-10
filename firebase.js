cfb5a6c7dec96ba0e2684e75631a1d50ea9893a9
João Victor Hummel Guidi. <joaovhguidi@gmail.com>
2026-05-09 23:08:39 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,20 @@
+ const firebaseConfig = {
+
+    apiKey: "AIzaSyDF-Tt8qDbX9Ln1bVJ0MTvKbq6rOFyK034",
+
+    authDomain: "jquery-firebase-list.firebaseapp.com",
+
+    projectId: "jquery-firebase-list",
+
+    storageBucket: "jquery-firebase-list.firebasestorage.app",
+
+    messagingSenderId: "72180246757",
+
+    appId: "1:72180246757:web:56400f648d0244c21e0c0c",
+
+    measurementId: "G-EX351TP8CK"
+
+  };
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
